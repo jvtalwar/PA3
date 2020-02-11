2482ce850c5f0126bc02375c5a55f221d3d86ce5
jvtalwar <42721746+jvtalwar@users.noreply.github.com>
2020-02-10 23:28:38 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 import torch 
-#import math 
+import math 
 
 def iou(pred, target, n_class): #pass in a 3d tensor of non one-hot encoded vectors --> number of images,H,W
     dontInclude = set([0,1,2,3,4,5,6,9,10,14,15,16,18,29,30]) #These are the classes with TrainId == 255
-    ious = []
+    #ious = []
+    intermission = [] # intersection
+    wasUpWithThat = [] #unions
     forCalculation = list()
     for cls in range(n_class):
 
         if cls in dontInclude:
-            ious.append(float('nan'))
+            intermission.append(float('nan'))
+            wasUpWithThat.append(float('nan'))
             continue 
          
         prediction = pred == cls
@@ -17,49 +20,22 @@ def iou(pred, target, n_class): #pass in a 3d tensor of non one-hot encoded vect
         intersection = (prediction & targeted).float().sum(dim =(1,2))
         union = (prediction | targeted).float().sum(dim = (1,2))
         
-        #if cls == 12:
-        #    print(intersection)
-        #    print(union)
-        #smoothCriminal = 0.000001
-        
-        if min(union).item() == 0: 
-            #some images don't have any pizels while some do - this will need to be fixed in the future once get metric clarity
-            
-            #print("This shouldn't be happening...." + str(cls)) 
-            ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
-        else:
-            
-            toAppend = (intersection/union) #(union+smoothCriminal)) #handle 0/0
-            
-            
-            #if math.isnan(toAppend):
-            #    print("This makes no sense..." + str(cls))
-            #    print(intersection)
-            #    print(union)
-            #print("Taking the sum:")
-            #print(sum(toAppend)
-            #print("getting the len:")
-            #print(len(toAppend))
-            
-            toAppend = sum(toAppend)/len(toAppend)
-            
-            #if math.isnan(toAppend):
-            #    print("This makes no sense..." + str(cls))
-            #    print(intersection)
-            #    print(union)
-            #    print(intersection/union)
-            
-            ious.append(toAppend.item())
-            forCalculation.append(toAppend.item())
-            
-    #print("doingThings!!!")
-    
-    #print(forCalculation)
-    
-    #return the ious of all classes and the mean iou of the batch; the former is done to return an indexed array to match 
-    #with specific classes IOU that can be accessed as requested in the assignment (e.g. building: 11)
-    
-    return torch.Tensor(ious), sum(forCalculation)/len(forCalculation)
+        wasUpWithThat.append(union.sum().item()) #for a particular class sum along the batch dimesnion
+        intermission.append(intersection.sum().item()) #sum the intersection along the batch dimension     
+           
+
+    return torch.Tensor(intermission), torch.Tensor(wasUpWithThat) # torch.Tensor(ious), sum(forCalculation)/len(forCalculation)
+
+
+def MeanIOU(combined):
+    numLoops = 0
+    runningSum = 0
+    for el in combined:
+        if math.isnan(el):
+            continue
+        numLoops += 1
+        runningSum += el.item()
+    return runningSum/numLoops
 
 
 def pixel_acc(pred, target):
