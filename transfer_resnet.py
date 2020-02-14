b5b079fed926a6aac04858d0a3a8509e4ae9cc49
kian kalhor <kkalhor@dsmlp-login.ucsd.edu>
2020-02-13 16:13:29 -0800

    no transfer yet

---
+++ transfer_resnet.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+import torchvision
+
+
+class FCN(nn.Module):
+
+    def __init__(self, n_class, base_model = 'resnet18', fine_tune = False):
+        super().__init__()
+        self.n_class = n_class
+        
+        self.decoder = torchvision.models.resnet18(pretrained = True)
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+        
+        self.base_mod.fc = nn.Linear(self.base_mod.fc.in_features, out_features = n_class)
+
+        self.conv1   = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, dilation=1)
+        self.bnd1    = nn.BatchNorm2d(32)
+        self.conv2   = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, dilation=1)
+        self.bnd2    = nn.BatchNorm2d(64)
+        self.conv3   = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, dilation=1)
+        self.bnd3    = nn.BatchNorm2d(128)
+        self.conv4   = nn.Conv2d(128,256, kernel_size=3, stride=2, padding=1, dilation=1)
+        self.bnd4    = nn.BatchNorm2d(256)
+        self.conv5   = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, dilation=1)
+        self.bnd5    = nn.BatchNorm2d(512)
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32,n_class, kernel_size=1)
+        
+    def forward(self, x):
+        
+        x = self.base_mod(x)
+        
+        return score  # size=(N, n_class, x.H/1, x.W/1)
