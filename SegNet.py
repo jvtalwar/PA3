5e4405aacc869a5fecb6e780a8f690b0a9736f5c
jvtalwar <42721746+jvtalwar@users.noreply.github.com>
2020-02-14 22:32:14 -0800

    Update SegNet.py

--- SegNet.py
+++ SegNet.py
@@ -24,7 +24,7 @@ class SegNet(nn.Module):
         self.conv3Two = nn.Conv2d(256, 256, kernel_size=3, padding=1, dilation=1)
         #self.conv3Three = nn.Conv2d(256, 256, kernel_size=3, padding=1, dilation=1)
         self.conv4One = nn.Conv2d(256, 512, kernel_size=3, padding=1, dilation=1)
-        self.conv4Two = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
+        #self.conv4Two = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
         #self.conv4Three = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
         #self.conv5One = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
         #self.conv5Two = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
@@ -34,7 +34,7 @@ class SegNet(nn.Module):
         #self.deconv5Two = nn.ConvTranspose(512, 512, kernel_size=3, padding=1, dilation=1)
         #self.deconv5Three = nn.ConvTranspose(512, 512, kernel_size=3, padding=1, dilation=1)
         self.deconv4One = nn.Conv2d(512, 256, kernel_size=3, padding=1, dilation=1)
-        self.deconv4Two = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
+        #self.deconv4Two = nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1)
         #self.deconv4Three = nn.ConvTranspose(512, 512, kernel_size=3, padding=1, dilation=1)
         self.deconv3One = nn.Conv2d(256, 128, kernel_size=3, padding=1, dilation=1)
         self.deconv3Two = nn.Conv2d(256, 256, kernel_size=3, padding=1, dilation=1)
@@ -42,7 +42,8 @@ class SegNet(nn.Module):
         self.deconv2One = nn.Conv2d(128, 64, kernel_size=3, padding=1, dilation=1)
         self.deconv2Two = nn.Conv2d(128, 128, kernel_size=3, padding=1, dilation=1)
         self.deconv1Two = nn.Conv2d(64, 64, kernel_size=3, padding=1, dilation=1)
-        self.deconv1One = nn.Conv2d(64, n_class, kernel_size=1)
+        self.deconv1One = nn.Conv2d(64, 64, kernel_size=3, padding=1, dilation=1)
+        self.finalConv = nn.Conv2d(64, n_class, kernel_size=1)
         
         #batchnorm
         self.bn1 = nn.BatchNorm2d(64)
@@ -87,7 +88,7 @@ class SegNet(nn.Module):
         x, thirdIndexes = self.mp3(x)
         
         x = self.relu(self.bn4and5(self.conv4One(x)))
-        x = self.relu(self.bn4and5(self.conv4Two(x)))
+        #x = self.relu(self.bn4and5(self.conv4Two(x)))
         #x = self.relu(self.bn4and5(self.conv4Three(x)))
         cuatro = x.size()
         x, fourthIndexes = self.mp4(x)
@@ -107,7 +108,7 @@ class SegNet(nn.Module):
         
         x = self.unpool4(x, fourthIndexes, output_size = cuatro)
         #x = self.relu(self.bn4and5(self.deconv4Three(x)))
-        x = self.relu(self.bn4and5(self.deconv4Two(x)))
+        #x = self.relu(self.bn4and5(self.deconv4Two(x)))
         x = self.relu(self.bn3(self.deconv4One(x)))
         
         x = self.unpool3(x, thirdIndexes, output_size = tres)
@@ -121,12 +122,7 @@ class SegNet(nn.Module):
         
         x = self.unpool1(x, firstIndexes, output_size = uno)
         x = self.relu(self.bn1(self.deconv1Two(x)))
-        x = self.deconv1One(x)
+        x = self.relu(self.bn1(self.deconv1One(x)))
+        x = self.finalConv(x)
         
         return x
-        
-        
-        
-        
-        
-        
